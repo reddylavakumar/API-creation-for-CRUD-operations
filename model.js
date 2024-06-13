f1f900eabeed80b3eae10c156c6fee31b445eea7
reddylavakumar <reddylavakumar2001@gmailcom>
2024-06-13 17:06:58 +0530

    update all the files correctly

---
+++ model.js
@@ -0,0 +1,15 @@
+// const express = require('express');
+const mongoose = require('mongoose');
+
+const BrandName = mongoose.Schema({
+    brandname: {
+        type : String,
+        required:true
+    },
+    date:{
+        type:Date,
+        default:Date.now
+    }
+})
+
+module.exports = mongoose.model('brandname',BrandName)
